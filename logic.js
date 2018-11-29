847fd529cdfec374781e17800fc546c103a98fee
Solal Cohen <solalcohen@MacBook-Pro-de-Solal.local>
2018-11-29 17:59:53 +0200

    first commit

---
+++ logic.js
@@ -0,0 +1,15 @@
+class Toolbar extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (   
+        )
+    }
+
+}
+
+ReactDOM.render(
+    <Toolbar />,
+    document.getElementById("root")
+);
