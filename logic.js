5c4764320b047474d668e32558cc4f3f558be3e8
Solal Cohen <solalcohen@MacBook-Pro-de-Solal.local>
2018-12-04 19:27:09 +0200

    add the input task

--- logic.js
+++ logic.js
@@ -7,7 +7,7 @@ class App extends React.Component {
             <div className='todolist'>
                 <Title bgcolor='red' text='TO DO LIST' font='black' source='https://img.icons8.com/metro/1600/todo-list.png' />
                 <div className='container'>
-                    <List day='Sunday' length='25' /><List day='Monday' length='25'/><List day='Tuesday' length='25'/><List day='Wednesday' length='25' /><List day='Thursday' length='25'/><List day='Friday' length='25'/><List day='Saturday' length='25'/>
+                    <List day='Sunday' length='25' /><List day='Monday' length='25' /><List day='Tuesday' length='25' /><List day='Wednesday' length='25' /><List day='Thursday' length='25' /><List day='Friday' length='25' /><List day='Saturday' length='25' />
                 </div>
             </div>
         )
@@ -19,10 +19,38 @@ class Title extends React.Component {
         super(props);
     }
     render() {
+        var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+        var days = [];
+        var years = [];
+        for (var i = 1; i <= 31; i++) {
+            days.push(i)
+        }
+        for (var i = 2018; i <= 2050; i++) {
+            years.push(i)
+        }
+        var daysDropdown = days.map(
+            x => <option key={`item${x}`}>{x}</option>);
+        var monthsDropdown = months.map(
+            x => <option key={`item${x}`}>{x}</option>);
+        var yearsDropdown = years.map(
+            x => <option key={`item${x}`}>{x}</option>);
         return (
             <div className='Title' style={{ backgroundColor: this.props.bgcolor, color: this.props.font }}>
                 <img src={this.props.source}></img>
                 <div>{this.props.text}</div>
+                <div className='add'>
+                    <input type='text' placeholder="Add a task" ref={(input) => { this.textInput = input; }} />
+                    <select ref={(select) => { this.daySelect = select; }}>
+                        {daysDropdown}
+                    </select>
+                    <select ref={(select) => { this.monthSelect = select; }}>
+                        {monthsDropdown}
+                    </select>
+                    <select ref={(select) => { this.yearSelect = select; }}>
+                        {yearsDropdown}
+                    </select>
+                    <button onClick={this.select}>Add</button><br />
+                </div>
             </div>
         )
     }
@@ -41,22 +69,11 @@ class List extends React.Component {
                     <span>Something for today?</span>
                     <br />
                     <br />
-                    <input maxLength={this.props.length} ></input>
-                    <input maxLength={this.props.length}></input>
-                    <input maxLength={this.props.length}></input>
-                    <input maxLength={this.props.length}></input>
-                    <input maxLength={this.props.length}></input>
-                    <input maxLength={this.props.length}></input>
-                    <input maxLength={this.props.length}></input>
-                    <input maxLength={this.props.length}></input>
-
                 </div>
                 <div className='done'>
                     <br />
+                    <span>You made it !</span>
                     <br />
-                    <input maxLength={this.props.length}></input>
-                    <input maxLength={this.props.length}></input>
-                    <input maxLength={this.props.length}></input>
                 </div>
             </div>
         )
