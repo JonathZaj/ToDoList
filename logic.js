cfb35f9f30b2395d58832e0f2da295bfc3434a11
Solal Cohen <solalcohen@MacBook-Pro-de-Solal.local>
2018-12-05 09:37:58 +0200

    other mvp

--- logic.js
+++ logic.js
@@ -1,43 +1,55 @@
 class App extends React.Component {
     constructor(props) {
         super(props);
-        this.writeTask=this.writeTask.bind(this)
-        this.state = {
-            tasks: []
-        }
-    }
-    writeTask(update){
-        var updatedTasks = this.state.tasks;
-        updatedTasks.push(update);
-        this.setState({
-            tasks: updatedTasks
-        })
     }
     render() {
         return (
             <div className='todolist'>
-                <Title bgcolor='red' text='TO DO LIST' font='black' source='https://img.icons8.com/metro/1600/todo-list.png' updateList={this.writeTask}/>
+                <InputBar bgcolor='rgba(0,0,0,0.5)' text='TO DO LIST' font='white' source='http://chittagongit.com//images/todo-list-icon/todo-list-icon-18.jpg' />
                 <div className='container'>
-                    <List addedTask={this.state.tasks}/>
+                    <List />
                 </div>
             </div>
         )
     }
 
 }
-class Title extends React.Component {
+class InputBar extends React.Component {
     constructor(props) {
         super(props);
-        this.add=this.add.bind(this)
     }
-    add(){
-        var day = this.daySelect.value;   
-        var month = this.monthSelect.value;   
-        var year = this.yearSelect.value;   
-        var input = this.textInput.value;
-        var task = `${input} on ${day}.${month}.${year}`;
-        this.props.updateList(task);   
+    render() {
 
+        return (
+            <div className='InputBar' style={{ backgroundColor: this.props.bgcolor, color: this.props.font }}>
+                <img src={this.props.source}></img>
+                <div>{this.props.text}</div>
+            </div>
+        )
+    }
+
+}
+class List extends React.Component {
+    constructor(props) {
+        super(props);
+        this.add = this.add.bind(this);
+        this.state = {
+            tasks: [],
+            key: 0
+        }
+    }
+    add() {
+        this.setState({
+            tasks: this.state.tasks,
+            key: this.state.key + 1
+
+        })
+        var day = this.daySelect.value;
+        var month = this.monthSelect.value;
+        var year = this.yearSelect.value;
+        var input = this.textInput.value;
+        var task = ` ${input} On ${day}.${month}.${year}`;
+        this.state.tasks.push(<li key={this.state.key + 1} className='list'><input type="checkbox"></input><div>{task}</div></li>)
     }
     render() {
         var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
@@ -56,46 +68,26 @@ class Title extends React.Component {
         var yearsDropdown = years.map(
             x => <option key={`item${x}`}>{x}</option>);
         return (
-            <div className='Title' style={{ backgroundColor: this.props.bgcolor, color: this.props.font }}>
-                <img src={this.props.source}></img>
-                <div>{this.props.text}</div>
-                <div className='add'>
-                    <input type='text' placeholder="Add a task" ref={(input) => { this.textInput = input; }} />
-                    <select ref={(select) => { this.daySelect = select; }}>
-                        {daysDropdown}
-                    </select>
-                    <select ref={(select) => { this.monthSelect = select; }}>
-                        {monthsDropdown}
-                    </select>
-                    <select ref={(select) => { this.yearSelect = select; }}>
-                        {yearsDropdown}
-                    </select>
-                    <button onClick={this.add}>Add</button><br />
-                </div>
-            </div>
-        )
-    }
-
-}
-class List extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        var list=this.props.addedTask.map(
-            x => <li key={`item${x}`}>{x}</li>
-        )
-        return (
             <div className='tododone' >
                 <div className='todo'>
                     <div>{this.props.day}</div>
                     <br />
-                    <span>Something for today?</span>
-                    <br />
+                    <div className='add'>
+                        <input type='text' placeholder=" Something new ? Add a task" ref={(input) => { this.textInput = input; }} />
+                        <select ref={(select) => { this.daySelect = select; }}>
+                            {daysDropdown}
+                        </select>
+                        <select ref={(select) => { this.monthSelect = select; }}>
+                            {monthsDropdown}
+                        </select>
+                        <select ref={(select) => { this.yearSelect = select; }}>
+                            {yearsDropdown}
+                        </select>
+                        <button onClick={this.add}>Add</button><br />
+                    </div>
                     <br />
                     <ul>
-                        {list}
+                        {this.state.tasks}
                     </ul>
                 </div>
                 <div className='done'>
